fda2ce48d969d743f2c8fec892b8099ba8640a16
Anant Aneja <1797669+aaneja@users.noreply.github.com>
2023-09-19 14:56:18 +0530

    Test 2 - Add more native tests overriding abstract base tests

---
+++ presto-native-execution/src/test/java/com/facebook/presto/nativeworker/origtests/TestTopN.java
@@ -0,0 +1,24 @@
+package com.facebook.presto.nativeworker.origtests;
+
+import com.facebook.presto.nativeworker.PrestoNativeQueryRunnerUtils;
+import com.facebook.presto.testing.ExpectedQueryRunner;
+import com.facebook.presto.testing.QueryRunner;
+import com.facebook.presto.tests.AbstractTestQueries;
+import com.facebook.presto.tests.AbstractTestTopNQueries;
+
+public class TestTopN
+        extends AbstractTestTopNQueries
+{
+    @Override
+    protected QueryRunner createQueryRunner() throws Exception
+    {
+        return PrestoNativeQueryRunnerUtils.createNativeQueryRunner(true);
+    }
+
+    @Override
+    protected ExpectedQueryRunner createExpectedQueryRunner() throws Exception
+    {
+        return PrestoNativeQueryRunnerUtils.createJavaQueryRunner();
+    }
+
+}
